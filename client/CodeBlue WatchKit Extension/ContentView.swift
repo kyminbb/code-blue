95ad137f1718d6d02663f55c799e15c565fdb528
k2sebeom <moses97@gmail.com>
2022-02-05 10:40:10 +0900

    Add client code skeleton

---
+++ client/CodeBlue WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeBlue WatchKit Extension
+//
+//  Created by Elcy on 2022/02/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
