d9525e19ca1aea9dd31f7d8142dc74acbc30846c
Kyungmin Kim <kyminbb.kim@gmail.com>
2022-02-04 23:24:02 -0500

    Implement endpoint for get_visitor

--- server/api/endpoints/visitors.py
+++ server/api/endpoints/visitors.py
@@ -17,3 +17,11 @@ async def register_visitor(visitor: Visitor, visitors_service: VisitorsService =
     if response.visitor_id == -1:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Visitor already exists")
     return response
+
+
+@router.get("/{visitor_id}", status_code=status.HTTP_200_OK, response_model=Visitor)
+async def get_visitor(visitor_id: int, visitors_service: VisitorsService = Depends(get_visitors_service)):
+    visitor = await visitors_service.get_visitor(visitor_id)
+    if not visitor:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Visitor does not exist")
+    return visitor
