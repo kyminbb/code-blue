8719dbf393ca030e24d8fff42296e3b2440c29d1
Kyungmin Kim <kyminbb.kim@gmail.com>
2022-02-05 16:41:53 -0500

    Fix bugs in DB connection

--- server/api/dependencies.py
+++ server/api/dependencies.py
@@ -1,12 +1,13 @@
 from functools import lru_cache
-from typing import AsyncGenerator
 
 from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncEngine
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 from server.core.config import Settings
+from server.models import Base
 from server.repositories.visitors_repository import VisitorsRepository
 from server.services.visitors_service import VisitorsService
 
@@ -16,26 +17,28 @@ def get_settings() -> Settings:
     return Settings()
 
 
-engine = create_async_engine(get_settings().DB_URL)
+engine: AsyncEngine = create_async_engine(get_settings().DB_URL, future=True)
+session_maker: sessionmaker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-async def close_db() -> None:
+async def start_db() -> None:
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def shutdown_db() -> None:
     await engine.dispose()
 
 
-@lru_cache()
-def _get_reusable_session_maker() -> sessionmaker:
-    return sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+async def get_session() -> AsyncSession:
+    async with session_maker() as session:
+        yield session
 
 
-async def get_session_maker() -> AsyncGenerator[sessionmaker, None]:
-    yield _get_reusable_session_maker()
-
-
-def get_visitors_repository(session_maker: sessionmaker = Depends(get_session_maker)) -> VisitorsRepository:
-    return VisitorsRepository(session_maker)
+def get_visitors_repository(session: AsyncSession = Depends(get_session)) -> VisitorsRepository:
+    return VisitorsRepository(session)
 
 
 def get_visitors_service(visitors_repository: VisitorsRepository = Depends(get_visitors_repository)) -> VisitorsService:
     return VisitorsService(visitors_repository)
-
--- server/repositories/visitors_repository.py
+++ server/repositories/visitors_repository.py
@@ -3,32 +3,31 @@ from typing import Optional
 from sqlalchemy import func
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from server.models.visitors import Visitor
 
 
 class VisitorsRepository:
-    def __init__(self, session_maker: sessionmaker):
-        self.async_session = session_maker
+    def __init__(self, session: AsyncSession):
+        self.session = session
 
     async def visitor_exists(self, name: str, section: str, seat: str) -> bool:
         statement = select((func.count())).where(Visitor.name == name and Visitor.section_seat == f"{section}_{seat}")
-        async with self.async_session() as session:
-            return await session.execute(statement).first()[0] == 1
+        result = await self.session.execute(statement)
+        return result.first()[0] == 1
 
     async def add_visitor(self, name: str, section: str, seat: str, consent: bool) -> int:
         visitor = Visitor(name=name, section_seat=f"{section}_{seat}", section=section, consent=consent)
-        async with self.async_session() as session:
-            session.add(visitor)
-            try:
-                await session.commit()
-                return visitor.id
-            except IntegrityError:
-                await session.rollback()
-                return -1
+        self.session.add(visitor)
+        try:
+            await self.session.commit()
+            return visitor.id
+        except IntegrityError:
+            await self.session.rollback()
+            return -1
 
-    async def get_visitor(self, visitor_id: int) -> Optional[Visitor]:
-        async with self.async_session() as session:
-            result = await session.execute(select(Visitor).where(Visitor.id == visitor_id))
-            return result.scalar()
+
+async def get_visitor(self, visitor_id: int) -> Optional[Visitor]:
+    result = await self.session.execute(select(Visitor).where(Visitor.id == visitor_id))
+    return result.scalar()
